c84395db763e6e65d36ebe983b6a148b2969e811
Mzia <mdivani_mg@mgtniip.ru>
2018-05-18 16:35:30 +0300

    init commit

---
+++ structs.go
@@ -0,0 +1,31 @@
+package geojson
+
+// GeometryType тип для констант - типов геометрии
+type GeometryType string
+
+const (
+	// Point ...
+	Point GeometryType = "Point"
+	// MultiPoint ...
+	MultiPoint GeometryType = "MultiPoint"
+	// LineString ...
+	LineString GeometryType = "LineString"
+	// MultiLineString ...
+	MultiLineString GeometryType = "MultiLineString"
+	// Polygon ...
+	Polygon GeometryType = "Polygon"
+	// MultiPolygon ...
+	MultiPolygon GeometryType = "MultiPolygon"
+)
+
+// Geometry structure for geometry
+type Geometry struct {
+	Type            GeometryType    `json:"type"`
+	Coordinates     interface{}     `json:"coordinates"`
+	Point           []float64       `json:"point,omitempty"`
+	MultiPoint      [][]float64     `json:"multipoint,omitempty"`
+	LineString      [][]float64     `json:"lineString,omitempty"`
+	MultiLineString [][][]float64   `json:"multiLineString,omitempty"`
+	Polygon         [][][]float64   `json:"polygon,omitempty"`
+	MultiPolygon    [][][][]float64 `json:"multiPolygon,omitempty"`
+}
