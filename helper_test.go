b59830cdc2e00aeea24e9a9ab59c86cda06199b0
mmdivani <mdivani_mg@mgtniip.ru>
2018-07-27 09:56:14 +0300

    TestPIPJordanCurveTheorem

--- helper_test.go
+++ helper_test.go
@@ -27,11 +27,65 @@ func TestBearing(t *testing.T) {
 	assert.True(Bearing(0.9733938460598921, 0.6565617535524124, 0.9745994309845489, 0.6559685344195972) == 6.0136365913744)
 }
 
-// func TestPIPJordanCurveTheorem(t *testing.T) {
+func TestPIPJordanCurveTheorem(t *testing.T) {
 
-// 	assert := assert.New(t)
+	assert := assert.New(t)
 
-// }
+	plg := NewPolygon([][][]float64{
+		[][]float64{
+
+			[]float64{37.568435668945305, 55.75899767604707},
+			[]float64{37.49702453613281, 55.71028613431717},
+			[]float64{37.53547668457031, 55.68590756259903},
+			[]float64{37.57598876953125, 55.65667193958129},
+			[]float64{37.68413543701172, 55.68048803720246},
+			[]float64{37.691001892089844, 55.71086639119248},
+			[]float64{37.68516540527344, 55.75320187033113},
+			[]float64{37.65907287597656, 55.7624747460818},
+			[]float64{37.57392883300781, 55.76846230662442},
+			[]float64{37.51213073730469, 55.761315757185166},
+			[]float64{37.495994567871094, 55.741414409152696},
+			[]float64{37.53856658935547, 55.70931902037369},
+			[]float64{37.58628845214844, 55.68784292538311},
+			[]float64{37.64019012451172, 55.696357383733535},
+			[]float64{37.661476135253906, 55.73638916286441},
+			[]float64{37.63160705566406, 55.74972397348254},
+			[]float64{37.562599182128906, 55.73116995298137},
+			[]float64{37.55950927734375, 55.70815845204154},
+			[]float64{37.61238098144531, 55.696163893908825},
+			[]float64{37.64362335205078, 55.72420992089696},
+			[]float64{37.620277404785156, 55.73213652597946},
+			[]float64{37.605857849121094, 55.74798490877827},
+			[]float64{37.58766174316406, 55.72150290667663},
+			[]float64{37.61100769042969, 55.71860232605969},
+			[]float64{37.568435668945305, 55.75899767604707}}})
+
+	feature := NewFeature()
+	feature.SetProperty("Polyg", "[][][]")
+	feature.SetID("000")
+	feature.SetGeometry(plg)
+
+	point000 := NewPoint([]float64{37.6728, 55.7772})
+	feature000 := NewFeature()
+	feature000.SetProperty("location", "Kremlin")
+	feature000.SetID("000000")
+	feature000.SetGeometry(point000)
+
+	y, x := GetPointCoordinates(feature000)
+	pol := feature.Geom.Coordinates
+	assert.True(PIPJordanCurveTheorem(y, x, pol) == false)
+
+	point001 := NewPoint([]float64{37.5945, 55.7509})
+	feature001 := NewFeature()
+	feature001.SetProperty("location", "Kremlin")
+	feature001.SetID("000001")
+	feature001.SetGeometry(point001)
+
+	y, x = GetPointCoordinates(feature001)
+	pol = feature.Geom.Coordinates
+	assert.True(PointInPolygon(feature001, feature) == true)
+
+}
 
 func TestPointInPolygon(t *testing.T) {
 
